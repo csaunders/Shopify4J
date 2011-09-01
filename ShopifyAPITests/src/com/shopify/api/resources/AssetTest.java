bb206fac47edea7083a152672aa13ca306f5ee8a
Chris Saunders <chris@christophersaunders.ca>
2011-08-31 20:44:55 -0400

    Fixed a syntax error that was probably introduced via some codegen script changes

--- ShopifyAPITests/src/com/shopify/api/resources/AssetTest.java
+++ ShopifyAPITests/src/com/shopify/api/resources/AssetTest.java
@@ -9,8 +9,8 @@ public class AssetTest extends FixturesTestCase {
 		
 		Asset result = reader.read(singleAsset, Asset.class).get(0);
 		{
-			assertEquals("2010-07-12T15:31:50-04:00", result.getCreated_at());
-			assertEquals("2010-07-12T15:31:50-04:00", result.getUpdated_at());
+			assertEquals("2010-07-12T15:31:50-04:00", result.getCreatedAt());
+			assertEquals("2010-07-12T15:31:50-04:00", result.getUpdatedAt());
 			assertEquals(null, result.getPublicUrl());
 			assertTrue(result.getValue().startsWith("<!-- LIST 3 PER ROW -->"));
 			assertEquals("templates/index.liquid", result.getKey());
